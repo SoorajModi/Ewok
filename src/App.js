1640710f27e8791adfef64078f5b11a002298308
SoorajModi <soomodi@gmail.com>
2021-03-25 20:59:55 -0400

    Add routing and 404 not found page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from "react";
-import Home from './components/Home'
+import Home from './components/Home';
+import NotFound from './components/NotFound';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import './App.css';
 
 function App() {
     return (
         <div className="App">
             <header className="App-header">
-                <Home/>
+                <Router basename={process.env.PUBLIC_URL}>
+                    <Switch>
+                        <Route exact path="/" component={Home}/>
+                        <Route path="*" component={NotFound}/>
+                    </Switch>
+                </Router>
             </header>
         </div>
     );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "@material-ui/core/Button";
+
+function NotFound() {
+    return (
+        <>
+            <h1>Not Found</h1>
+            <Button href={'/'}>Home</Button>
+        </>
+    )
+}
+
+export default NotFound;
